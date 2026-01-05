9cf89cdde0c4c663e7a4a1d45797102e6d57c152
Dangar Othniel Djilouba <dangar-othniel.djilouba.1@ulaval.ca>
2026-01-05 18:27:11 -0500

    Cleanup: remove debug prints

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -11,5 +11,3 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-print("DATABASE_URL:", repr(DATABASE_URL))
-
